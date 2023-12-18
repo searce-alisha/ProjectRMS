8bdc611ed96bdca532982f5281dcf581bc2f9ad3
sealisha <alisha.walunj@searce.com>
2023-12-18 07:32:29 +0000

    changed build file

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@ OUTPUT=$(git show HEAD --name-only | grep Indigo/)
 # if [[ "$OUTPUT" == "Bookings" ]]; then 
 #          echo "Booking files have changed"
 # fi
-if [ -n "$OUTPUT" ]; then 
-  echo "Booking files have changed"
+if [ -n "$(git show HEAD --name-only | grep Bookings/)" ]; then 
+    echo "Booking files have changed"
+elif [ -n "$(git show HEAD --name-only | grep Fares/)" ]; then 
+    echo "Fares files have changed" 
 fi
